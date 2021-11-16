8648c2b3aca53fee673200f569fb04e6b1a2b88d
Saipul Bahri <91308052+saipulbahriert@users.noreply.github.com>
2021-11-16 12:09:23 +0700

    Update uts.py

--- UTS/uts.py
+++ UTS/uts.py
@@ -1,62 +1,111 @@
-# lulus =input("Apakah Kamu lulus")
-# if lulus == "Tidak":
-#     print("kamu tidak lulus")
+print("\n    ====Caffe GoodDay====")
 
-# elif lulus == "Iya":
-#     print("Kamu Lulus")
+pembeli = input("Masukkan nama Pembeli: ")
+print ("Nama Pembeli :", pembeli) 
 
-print("\n======  Rental Mobil  =======")
-print("Menyediakan Sewa Mobil Pribadi")
+total1=0
+jenis1=""
+porsi=0
+gelas=0
 
-mobil = input("Silahkan Masukkan nama anda : ")
-print("Nama Penyewa = ", mobil)
+def Daftar_barang():
+   global total1
+   global porsi
+   global jenis1
+   print ("\n========Menu Makanan=============")       
+   print("1. Burger King         : Rp25000")  
+   print("2. Spaghetti deluxe    : Rp20000")
+   print("3. Steak               : Rp50000")
+   print("4. Seblak              : Rp12000")
+   print("5. Ayam pedas          : Rp15000")
+   nomor=int(input("Masukan Pilihan: "))
+   porsi= int(input("Berapa Porsi: "))
+   
+   if nomor==1:
+       total1=porsi*25000
+       print (porsi," Burger King = Rp", total1)
+       jenis1=("Burger King")
+   elif nomor==2:
+       total1=porsi*20000
+       print (porsi," Spaghetti deluxe = Rp", total1)
+       jenis1=("Spaghetti deluxe")
+   elif nomor==3:
+       total1=porsi*50000
+       print (porsi, " Steak = Rp", total1)
+       jenis1=("Steak")
+   elif nomor==4:
+       total1=porsi*12000
+       print (porsi, " Seblak = Rp", total1)
+       jenis1=("Seblak")
+   elif nomor==5:
+       total1=porsi*15000
+       print (porsi, " Ayam pedas = Rp", total1)
+       jenis1=("Ayam pedas")
+   else:
+      print("Pilihan tidak tersedia, silahkan coba lagi!!")
+      Daftar_barang()
 
 
+Daftar_barang()
 
-saja = "lanjut"
-while(True) :
-    nama = [" Toyota Alphard 1 ", " Avanza 2"  , "Pajero Sport 3", "Fortuner 4" , "Honda Civic 5" ]
+total2=0
+jenis2=""
 
-    for list in nama:
-        print("\n==== 1. ", nama[0],"====")
-        print("\n==== 2. ", nama[1],"====")
-        print("\n==== 3. ", nama[2],"====") 
-        print("\n==== 4. ", nama[3],"====") 
-        print("\n==== 5. ", nama[4],"====")   
-        break
+def fungsiminuman():
+   global total2
+   global jenis2
+   global gelas
+   print("\n========Menu Minuman========")
+   print("1. HOT/ICE Ameericano   : Rp20000")
+   print("2. HOT/ICE Latte        : Rp15000")
+   print("3. HOT/ICE Matcha       : Rp15000")
+   print("4. HOT/ICE Tea          : Rp8000")
+   print("5. HOT/ICE Milk         : Rp10000")
+   nomor=int(input("Masukan Pilihan: "))
+   gelas= int(input("Berapa Gelas: "))
 
-    sewa = int(input("Silahkan Pilih Mobil yang akan di sewa : "))
-    lama = int(input("Berapa Hari : " ))
+   if nomor==1:
+       total2=gelas*20000
+       print (gelas," HOT/ICE Ameericano  = Rp", total2)
+       jenis2=(" HOT/ICE Ameericano ")
+   elif nomor==2:
+       total2=gelas*15000
+       print (gelas, " HOT/ICE Latte = Rp", total2)
+       jenis2=(" HOT/ICE Latte")
+   elif nomor==3:
+       total2=gelas*15000
+       print (gelas, " HOT/ICE Matcha  = Rp", total2)
+       jenis2=(" HOT/ICE Matcha ")
+   elif nomor==4:
+       total2=gelas*8000
+       print (gelas, " HOT/ICE Tea = Rp", total2)
+       jenis2=(" HOT/ICE Tea")
+   elif nomor==5:
+       total2=gelas*10000
+       print (gelas, " HOT/ICE Milk = Rp", total2)
+       jenis2=(" HOT/ICE Milk")
+   else:
+      print("Pilihan tidak tersedia, silahkan coba lagi!!")
+      fungsiminuman()
 
-    
-    if sewa == 1:
-        biaya1= lama*80000000
-        print("Biaya sewa Mobil 1 Rp : ",biaya1 )
 
-    elif sewa == 2:
-        biaya2 = lama*15000000
-        print("Biaya Sewa Mobil 2 Rp : ", biaya2)
+fungsiminuman()
+totalsemua=0
+totalsemua=total1+total2
+print("\nTotal yang harus dibayar: Rp",totalsemua)
+uang=int(input("Uang Tunai Pembeli: Rp."))
+kembalian=int(uang-totalsemua)
+print("Kembalian :",kembalian)
 
-    elif sewa == 3:
-        biaya3 = lama*23000000
-        print("Biaya Sewa Mobil 3 Rp : ", biaya3)
-
-    elif sewa == 4:
-        biaya4 = lama*30000000
-        print("Biaya Sewa Mobil 4 Rp : ", biaya4)
-
-    elif sewa == 5:
-        biaya5 = lama*45000000
-        print("Biaya Sewa Mobil 5 Rp : ", biaya5)
-    else:
-        print('masukan pilihan yang benar')
-        continue
-
-    lagi = (input("Apakah anda ingin menyewa mobil type lainnya  ? "))
-    if lagi == "y":
-           continue
-    else:
-        print("\n=====Terima Kasih Telah Berkunjung=====")
-        break
-
-    
+print("\n==================================")
+print("=========== T O T A L ============")
+print("==================================")
+print (" Nama          :",pembeli)
+print (" Barang        :",porsi,jenis1,"Rp.", total1)
+print ("                ",gelas,jenis2,"Rp.", total2)
+print (" Total         : Rp.",totalsemua)
+print (" Tunai         : Rp.",uang)
+print (" Kembalian     : Rp.",kembalian)
+print("=================================")
+print("==Terima Kasih Telah Berkunjung==")
+print("=================================")
